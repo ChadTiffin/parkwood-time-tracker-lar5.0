4a5c1128383623d8088f1425ec0535ddb005b748
Chad Tiffin <chad@chadtiffin.com>
2016-09-06 13:59:35 -0400

    Update title to Laravel 5.2.45

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Time Tracker Laravel 5.0</title>
+<title>Time Tracker on Laravel 5.2.45</title>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
